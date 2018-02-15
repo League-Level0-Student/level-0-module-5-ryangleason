f5f0a25cf71211f653d50367ef2e87fd31e0c8f5
ryangleason <league@iMac-2.attlocal.net>
2018-02-14 19:33:39 -0800

    Made a goofy names thing and checking for how many hours you coded

--- src/Code4Life.java
+++ src/Code4Life.java
@@ -7,11 +7,15 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-
+import javax.swing.JOptionPane;
+//Completed %100
 
 public class Code4Life {
 	/*
      * Ask the user how many hours they spent coding this week.
+     * 
+     *
+     * 
      * 
      * 1. If it's 3 or more, tell them they're a Code Ninja.
      * 
@@ -19,7 +23,15 @@ public class Code4Life {
      * 
      * 3. If it's more than 5, play the Batman theme song.
      */
-
+public static void main(String[] args) {
+String hours = JOptionPane.showInputDialog("How many hours did you spend coding last week?");
+	int hoursInt = Integer.parseInt(hours);
+	if (hoursInt == 3 || hoursInt > 3) { playBatmanTheme();
+	
+		
+	}
+	else {JOptionPane.showMessageDialog(null, "stop watching youtube and start coding, (time is money)");}
+}
 
     private static void playBatmanTheme() {
     	try {
--- src/GoofyNames.java
+++ src/GoofyNames.java
@@ -10,7 +10,8 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-
+String name = JOptionPane.showInputDialog("What is you name?");
+System.out.println(name.toUpperCase());
 		// 2. Print upper case name to the console using .toUpperCase()
 		//    Run your program to see that this works.
 
@@ -18,18 +19,28 @@ public class GoofyNames {
 		//    HINT: Use .length() to determine the number of characters in the String.
 
 			// 4. Create a String variable to store the next character of the name
+for (int i = 0; i < name.length() ; i++) {
+	String letters = ""+name.charAt(i);
+	System.out.println(letters);
+
+
+
 			//    using .substring(start, end)
 			//    HINT: replace 'start' and 'end' to get String with character at i 
 			
 			// 5. Use MODULO to set this variable to upper case for EVEN characters
 			//    and lower case for ODD characters.
+	
+if (i % 2== 0 ) {goofyName += letters.toLowerCase();}else { goofyName +=letters.toUpperCase();}
 
-			
+}
+
+System.out.println(goofyName);
+			JOptionPane.showMessageDialog(null, "Your 'goofy name' is: " + goofyName + "!");
 			// 6. ADD this String  (containing 1 char) to the goofyName String
 
 		
 		// 7. Use pop-up to show user their Goofy name
-
-	}
 }
+	}
 
